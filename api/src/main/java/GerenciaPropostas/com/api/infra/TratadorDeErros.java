79fe73d6abb5e3db5091030b77b0a7296a9e228d
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-12 15:27:50 -0300

    adicionado a classe TratadorDeErros para centralizar o tratamento de excessões nos controladores de requisições REST, melhorando a legibilidade das mensagens de erro no resposta dos metodos

---
+++ api/src/main/java/GerenciaPropostas/com/api/infra/TratadorDeErros.java
@@ -0,0 +1,43 @@
+package GerenciaPropostas.com.api.infra;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity tratarErro404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+		var erros = exception.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toList());
+	}
+	
+	@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+	public ResponseEntity tratarSqlException(SQLIntegrityConstraintViolationException exception) {
+		var mensagem = exception.getMessage();
+//		System.out.println("tratarSqlException: "+mensagem);
+		return ResponseEntity.badRequest().body(mensagem);
+	}
+	
+	private record DadosErrosValidacao(String campo, String mensagem) {
+		public DadosErrosValidacao(FieldError fieldError) {
+			this(fieldError.getField(),fieldError.getDefaultMessage());
+		}
+	}
+	
+	
+	
+}
